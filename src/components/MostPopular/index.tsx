082a724a4a2d5fca0c791e793d815231c9927d83
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-03-20 01:20:16 -0300

    Feat: starting to stylize the cards

--- src/components/MostPopular/index.tsx
+++ src/components/MostPopular/index.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { Bottom, Container, Top } from "./components/Card";
 import Image from "next/image";
+import recipes from "./mocks";
 
 export default function MostPopular() {
   return (
-    <Container>
-      <Top>
-        <Image src="" alt="" />
-      </Top>
-      <Bottom>
-        <h1>Waffles</h1>
-        <p>Top Recipe</p>
-        <button>See Recipe</button>
-      </Bottom>
-    </Container>
+    <main className="my-12 flex flex-col gap-14">
+      <h1 className="text-center font-bold text-slate-900 text-4xl">
+        Most Popular Recipes
+      </h1>
+      <div className="flex justify-center items-center gap-3">
+        {recipes.map(({ img, name, description, id }) => (
+          <Container key={id} className="max-w-[300px]">
+            <Top>
+              <Image src={img} alt="image of recipe" />
+            </Top>
+            <Bottom>
+              <h1>{name}</h1>
+              <p>{description}</p>
+              <button>See Recipe</button>
+            </Bottom>
+          </Container>
+        ))}
+      </div>
+    </main>
   );
 }
---
+++ src/components/MostPopular/mocks/index.ts
@@ -0,0 +1,26 @@
+import waffles from "../../../assets/CardImgs/mostPopularImgs/Waffles.jpeg";
+import pancakles from "../../../assets/CardImgs/mostPopularImgs/Pancakles.jpeg";
+import hotChocolate from "../../../assets/CardImgs/mostPopularImgs/HotChocolate.jpeg";
+
+const recipes = [
+  {
+    id: "1",
+    name: "Waffles",
+    description: "Cool recipe to eat with friends in the morning",
+    img: waffles,
+  },
+  {
+    id: "2",
+    name: "Pancakes",
+    description: "cool recipe to eat with friends in the morning",
+    img: pancakles,
+  },
+  {
+    id: "3",
+    name: "Hot Chocolate",
+    description: "cool recipe to eat with friends in the morning",
+    img: hotChocolate,
+  },
+];
+
+export default recipes;

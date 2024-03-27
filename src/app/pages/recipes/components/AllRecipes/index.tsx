ff3afa58c3101dbd7b0d26bdcd6978c074b6e72b
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-03-26 23:56:21 -0300

    Feat: Basic styles of th texts

--- src/app/pages/recipes/components/AllRecipes/index.tsx
+++ src/app/pages/recipes/components/AllRecipes/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function AllRecipes() {
-  return <div>AllRecipes</div>;
+  return (
+    <main>
+      <div>
+        <h1 className="text-center font-bold text-slate-900 text-4xl">
+          All Recipes
+        </h1>
+        <div></div>
+      </div>
+      <div>
+        <h1 className="text-center font-bold text-slate-900 text-4xl">
+          Latest Recipes
+        </h1>
+        <div></div>
+      </div>
+    </main>
+  );
 }

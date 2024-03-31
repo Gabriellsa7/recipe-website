bf959dc64044cef9f03972c78dd84e1e484d8e30
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-03-31 13:56:49 -0300

    Feat: simplified printing method

--- src/app/pages/recipePageInformation/components/Ingredients/index.tsx
+++ src/app/pages/recipePageInformation/components/Ingredients/index.tsx
@@ -7,33 +7,15 @@ export default function Ingredients() {
     <div>
       <h1>Ingredients</h1>
       <div>
-        {recipes.map((recipes) => (
-          <ul key={recipes.id}>
-            <li>{"\n" + recipes.ingredients?.at(0)}</li>
-            <li>{"\n" + recipes.ingredients?.at(1)}</li>
-            <li>{"\n" + recipes.ingredients?.at(2)}</li>
-            <li>{"\n" + recipes.ingredients?.at(3)}</li>
-            <li>{"\n" + recipes.ingredients?.at(4)}</li>
-            <li>{"\n" + recipes.ingredients?.at(5)}</li>
-            <li>{"\n" + recipes.ingredients?.at(6)}</li>
-            <li>{"\n" + recipes.ingredients?.at(7)}</li>
-            <li>{"\n" + recipes.ingredients?.at(8)}</li>
-            <li>{"\n" + recipes.ingredients?.at(9)}</li>
+        {recipes.map((recipe) => (
+          <ul key={recipe.id}>
+            {recipe.ingredients &&
+              recipe.ingredients.map((ingredient, i) => (
+                <li key={i}>{"\n" + ingredient}</li>
+              ))}
           </ul>
         ))}
       </div>
     </div>
   );
-}
-
-{
-  /* <li>2 ¼ cups all-purpose flour</li>
-<li>1 tablespoon baking powder</li>
-<li>3 tablespoons sugar</li>
-<li>½ teaspoon salt</li>
-<li>1 teaspoon cinnamon</li>
-<li>2 large eggs (separated)</li>
-<li>½ cup vegetable oil</li>
-<li>2 cups milk</li>
-<li>1 teaspoon vanilla extract</li> */
 }

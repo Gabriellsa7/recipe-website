50335e9c86f914e91026f02c53204d1fe6f12a6c
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-08 06:58:31 -0300

    Feat: creating the component where information about the preparation mode will be located

---
+++ src/app/pages/recipePageInformation/components/PreparationMode/mocks/index.ts
@@ -0,0 +1,56 @@
+//trying to put the ingredients recipes in an array of string
+const wafflesPreparationMode = {
+  preparationMode: [],
+};
+const pancakesPreparationMode = {
+  preparationMode: [],
+};
+const hotChocolatePreparationMode = {
+  preparationMode: [],
+};
+
+const donutPreparationMode = {
+  preparationMode: [],
+};
+
+const cerealPreparationMode = {
+  preparationMode: [],
+};
+
+const cookiesPreparationMode = {
+  preparationMode: [],
+};
+
+const mushroomRisottoPreparationMode = {
+  preparationMode: [],
+};
+
+const shrimpPadThaiPreparationMode = {
+  preparationMode: [],
+};
+
+const spaghettiCarbonaraPreparationMode = {
+  preparationMode: [],
+};
+
+const thaiGreenCurryPreparationMode = {
+  preparationMode: [],
+};
+
+const vegetableStirFryPreparationMode = {
+  preparationMode: [],
+};
+
+export {
+  cerealPreparationMode,
+  cookiesPreparationMode,
+  donutPreparationMode,
+  hotChocolatePreparationMode,
+  mushroomRisottoPreparationMode,
+  pancakesPreparationMode,
+  shrimpPadThaiPreparationMode,
+  spaghettiCarbonaraPreparationMode,
+  thaiGreenCurryPreparationMode,
+  vegetableStirFryPreparationMode,
+  wafflesPreparationMode,
+};

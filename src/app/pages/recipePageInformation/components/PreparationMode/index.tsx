668c26b289d1c7ad0e6ae7e2c2908a805ea45b29
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-08 06:53:11 -0300

    Feat: starting to create the preparation mode

--- src/app/pages/recipePageInformation/components/PreparationMode/index.tsx
+++ src/app/pages/recipePageInformation/components/PreparationMode/index.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import recipes, {
+  RecipeProps,
+} from "@/app/pages/initialPage/components/MostPopular/mocks";
+import recentRecipes from "@/app/pages/initialPage/components/RecentRecipe/mocks";
+import allRecipes from "@/app/pages/recipes/components/AllRecipes/mocks";
+import latestRecipes from "@/app/pages/recipes/components/LatestRecipes/mocks";
+import React, { useEffect, useState } from "react";
 
 export default function PreparationMode() {
+  const [recipe, setRecipe] = useState<RecipeProps | null>(null);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get("name") || "";
+
+    const generalRecipes = recipes
+      .concat(recentRecipes)
+      .concat(allRecipes)
+      .concat(latestRecipes);
+
+    const foundRecipe = generalRecipes.find((recipe) => recipe.name === name);
+
+    if (foundRecipe) {
+      setRecipe(foundRecipe);
+    } else {
+      console.error("Recipe not found");
+    }
+  }, []);
+
+  if (!recipe?.name) {
+    return <main>Loading...</main>;
+  }
   return (
     <div>
       <h1 className="font-bold text-2xl">Preparation Mode</h1>
+      <div className="mx-8">
+        <ul className="font-medium">
+          {/* {recipe.instructions &&
+            recipe.instructions.map((instruction, i) => (
+              <li key={i}>
+                <p className="text-lg">{instruction}</p>
+              </li>
+            ))} */}
+        </ul>
+      </div>
     </div>
   );
 }

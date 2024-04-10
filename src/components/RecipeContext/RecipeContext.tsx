040756286902857a3b4afbe164561b197959fe01
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-10 07:30:11 -0300

    Feat: created the RecipeContext component

---
+++ src/components/RecipeContext/RecipeContext.tsx
@@ -0,0 +1,44 @@
+import { StaticImageData } from "next/image";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  FC,
+  ReactNode,
+} from "react";
+
+interface Recipe {
+  id: number;
+  name: string;
+  description: string;
+  img: StaticImageData;
+}
+
+interface RecipeContextType {
+  recipes: Recipe[];
+  addRecipe: (newRecipe: Recipe) => void;
+}
+
+const RecipeContext = createContext<RecipeContextType | undefined>(undefined);
+
+export const useRecipe = () => {
+  const context = useContext(RecipeContext);
+  if (!context) {
+    throw new Error("useRecipes must be used within a RecipesProvider");
+  }
+  return context;
+};
+
+export const RecipeProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const addRecipe = (newRecipe: Recipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
+
+  return (
+    <RecipeContext.Provider value={{ recipes, addRecipe }}>
+      {children}
+    </RecipeContext.Provider>
+  );
+};

50318320608a17b1986dc18a5769e4d9aa443cfe
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-10 06:48:13 -0300

    Feat: make responsiveness with tailwind

--- src/app/pages/myRecipes/page.tsx
+++ src/app/pages/myRecipes/page.tsx
@@ -1,11 +1,14 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import React from "react";
 
 export default function MyRecipes() {
   return (
+    // make responsiveness with tailwind
     <main>
       <Header />
       <div></div>
+      <Footer />
     </main>
   );
 }

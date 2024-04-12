9d85a62a437594477204e5ae5013dfbc9cf090e9
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-12 11:18:18 -0300

    Feat: modifying the RecipeContext component

--- src/components/RecipeContext/RecipeContext.tsx
+++ src/components/RecipeContext/RecipeContext.tsx
@@ -1,12 +1,5 @@
-"use client";
 import { StaticImageData } from "next/image";
-import React, {
-  createContext,
-  useState,
-  useContext,
-  FC,
-  ReactNode,
-} from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface Recipe {
   id: number;
@@ -15,31 +8,48 @@ interface Recipe {
   img: StaticImageData;
 }
 
-interface RecipeContextType {
+interface RecipeContextData {
   recipes: Recipe[];
   addRecipe: (newRecipe: Recipe) => void;
+  removeRecipe: (id: number) => void;
 }
 
-const RecipeContext = createContext<RecipeContextType | undefined>(undefined);
+const RecipeContext = createContext<RecipeContextData | undefined>(undefined);
 
 export const useRecipe = () => {
   const context = useContext(RecipeContext);
   if (!context) {
-    throw new Error("useRecipes must be used within a RecipesProvider");
+    throw new Error("useRecipe must be used within a RecipeProvider");
   }
   return context;
 };
 
-export const RecipeProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
+export const RecipeProvider = (props: any) => {
+  const { children } = props;
+
+  const [recipes, setRecipes] = useState<Recipe[]>(() => {
+    const recipesLocalStorage = localStorage.getItem("recipes");
+    return recipesLocalStorage ? JSON.parse(recipesLocalStorage) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+  }, [recipes]);
 
   const addRecipe = (newRecipe: Recipe) => {
     setRecipes([...recipes, newRecipe]);
   };
 
+  const removeRecipe = (id: number) => {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  };
+
+  const value: RecipeContextData = {
+    recipes,
+    addRecipe,
+    removeRecipe,
+  };
   return (
-    <RecipeContext.Provider value={{ recipes, addRecipe }}>
-      {children}
-    </RecipeContext.Provider>
+    <RecipeContext.Provider value={value}>{children}</RecipeContext.Provider>
   );
 };

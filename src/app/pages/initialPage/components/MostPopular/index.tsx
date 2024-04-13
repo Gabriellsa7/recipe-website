b300aa5a77121299db05cf2fa3c7bf2ecd039438
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-13 11:19:59 -0300

    Feat: styling the myRecipes page

--- src/app/pages/initialPage/components/MostPopular/index.tsx
+++ src/app/pages/initialPage/components/MostPopular/index.tsx
@@ -24,42 +24,42 @@ export default function MostPopular() {
       </h1>
       <div className="flex justify-center items-center gap-10">
         {recipes.map((recipes) => (
-          // <Link
-          //   key={recipes.id}
-          //   href={`/pages/recipePageInformation?name=${recipes.name}&description=${recipes.description}&img=${recipes.img}`}
-          // >
-          <Container
+          <Link
             key={recipes.id}
-            className="max-w-[300px] max-h-[420px] bg-slate-200 rounded-2xl flex flex-col duration-500 hover:scale-110 cursor-pointer"
+            href={`/pages/recipePageInformation?name=${recipes.name}&description=${recipes.description}&img=${recipes.img}`}
           >
-            <Top>
-              <Image
-                className="w-[300px] h-[190px] rounded-t-2xl"
-                src={recipes.img}
-                alt="image of recipe"
-              />
-            </Top>
-            <Bottom className="flex flex-col gap-5 items-start mx-6 my-4 pb-4">
-              <h1 className="text-2xl font-bold text-slate-800">
-                {recipes.name}
-              </h1>
-              <p className="font-medium text-lg text-slate-900">
-                {recipes.description}
-              </p>
-              <div className="flex gap-20 w-full items-center">
-                <button className="bg-yellow-400 text-slate-900 px-6 py-1.5 rounded-full font-bold text-lg hover:bg-yellow-300 duration-500">
-                  See Recipe
-                </button>
-                <button onClick={() => handleAddFavorites(recipes)}>
-                  <FaBookmark
-                    className="text-[#3e404a] hover:text-slate-400 duration-500"
-                    size={25}
-                  />
-                </button>
-              </div>
-            </Bottom>
-          </Container>
-          // </Link>
+            <Container
+              key={recipes.id}
+              className="max-w-[300px] max-h-[420px] bg-slate-200 rounded-2xl flex flex-col duration-500 hover:scale-110 cursor-pointer"
+            >
+              <Top>
+                <Image
+                  className="w-[300px] h-[190px] rounded-t-2xl"
+                  src={recipes.img}
+                  alt="image of recipe"
+                />
+              </Top>
+              <Bottom className="flex flex-col gap-5 items-start mx-6 my-4 pb-4">
+                <h1 className="text-2xl font-bold text-slate-800">
+                  {recipes.name}
+                </h1>
+                <p className="font-medium text-lg text-slate-900">
+                  {recipes.description}
+                </p>
+                <div className="flex gap-20 w-full items-center">
+                  <button className="bg-yellow-400 text-slate-900 px-6 py-1.5 rounded-full font-bold text-lg hover:bg-yellow-300 duration-500">
+                    See Recipe
+                  </button>
+                  <button onClick={() => handleAddFavorites(recipes)}>
+                    <FaBookmark
+                      className="text-[#3e404a] hover:text-slate-400 duration-500"
+                      size={25}
+                    />
+                  </button>
+                </div>
+              </Bottom>
+            </Container>
+          </Link>
         ))}
       </div>
     </main>
--- src/app/pages/myRecipes/page.tsx
+++ src/app/pages/myRecipes/page.tsx
@@ -7,6 +7,7 @@ import {
   useRecipe,
 } from "@/components/RecipeContext/RecipeContext";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function MyRecipes() {
@@ -24,33 +25,42 @@ function MyRecipesContent() {
   return (
     <main>
       <Header />
-      {recipes.map((recipe) => (
-        <Container
-          key={recipe.id}
-          className="max-w-[300px] max-h-[420px] bg-slate-200 rounded-2xl flex flex-col duration-500 hover:scale-110 cursor-pointer"
-        >
-          <Top>
-            {recipe.img && (
-              <Image
-                className="w-[300px] h-[190px] rounded-t-2xl"
-                src={recipe.img}
-                alt="image of recipe"
-              />
-            )}
-          </Top>
-          <Bottom className="flex flex-col gap-5 items-start mx-6 my-4 pb-4">
-            <h1 className="text-2xl font-bold text-slate-800">{recipe.name}</h1>
-            <p className="font-medium text-lg text-slate-900">
-              {recipe.description}
-            </p>
-            <div className="flex gap-20 w-full items-center">
-              <button className="bg-yellow-400 text-slate-900 px-6 py-1.5 rounded-full font-bold text-lg hover:bg-yellow-300 duration-500">
-                See Recipe
-              </button>
-            </div>
-          </Bottom>
-        </Container>
-      ))}
+      <section className="grid grid-cols-4 justify-center items-center gap-10 m-20">
+        {recipes.map((recipe) => (
+          <Link
+            key={recipe.id}
+            href={`/pages/recipePageInformation?name=${recipe.name}&description=${recipe.description}&img=${recipe.img}`}
+          >
+            <Container
+              key={recipe.id}
+              className="max-w-[300px] max-h-[420px] bg-slate-200 rounded-2xl flex flex-col duration-500 hover:scale-110 cursor-pointer"
+            >
+              <Top>
+                {recipe.img && (
+                  <Image
+                    className="w-[300px] h-[190px] rounded-t-2xl"
+                    src={recipe.img}
+                    alt="image of recipe"
+                  />
+                )}
+              </Top>
+              <Bottom className="flex flex-col gap-5 items-start mx-6 my-4 pb-4">
+                <h1 className="text-2xl font-bold text-slate-800">
+                  {recipe.name}
+                </h1>
+                <p className="font-medium text-lg text-slate-900">
+                  {recipe.description}
+                </p>
+                <div className="flex gap-20 w-full items-center">
+                  <button className="bg-yellow-400 text-slate-900 px-6 py-1.5 rounded-full font-bold text-lg hover:bg-yellow-300 duration-500">
+                    See Recipe
+                  </button>
+                </div>
+              </Bottom>
+            </Container>
+          </Link>
+        ))}
+      </section>
 
       <Footer />
     </main>

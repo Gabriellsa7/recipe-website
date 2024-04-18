d3f2160dd355079829b26c65872ff3d42d0bf908
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-18 09:10:25 -0300

    modifying the font of card texts in the RecentRecipe component

--- src/app/pages/initialPage/components/RecentRecipe/index.tsx
+++ src/app/pages/initialPage/components/RecentRecipe/index.tsx
@@ -29,10 +29,10 @@ export default function RecentRecipes() {
                 />
               </Top>
               <Bottom className="flex flex-col gap-5 items-start mx-6 my-4 pb-4 justify-around min-[768px]:min-h-[200px]">
-                <h1 className="text-2xl font-bold text-slate-800">
+                <h1 className="text-2xl font-bold text-slate-800 min-[320px]:text-lg min-[768px]:text-xl min-[1440px]:text-2xl">
                   {recentRecipes.name}
                 </h1>
-                <p className="font-medium text-lg text-slate-900 min-[320px]:max-w-[100px] min-[320px]:truncate min-[768px]:max-w-[200px] min-[768px]:truncate min-[768px]:overflow-hidden min-[1024px]:min-w-[500px]">
+                <p className="font-medium text-lg text-slate-900 min-[320px]:max-w-[100px] min-[320px]:truncate min-[768px]:max-w-[200px] min-[768px]:truncate min-[768px]:overflow-hidden min-[1024px]:min-w-[500px] min-[320px]:text-sm min-[768px]:text-lg">
                   {recentRecipes.description}
                 </p>
                 <button className="bg-yellow-400 text-slate-900 px-6 py-1.5 rounded-full font-bold text-lg hover:bg-yellow-300 duration-500">
